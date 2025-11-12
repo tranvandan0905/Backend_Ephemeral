9275aef055219408219af1155f3a0044d3744cc7
tranvandan0905 <dantranvan509@gmail.com>
2025-11-12 20:04:11 +0700

    fix  token

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,9 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-
+      const token = req.cookies.token;
     if (!token) {
         return res.status(401).json({ message: "Vui lòng đăng nhập!" });
     }
