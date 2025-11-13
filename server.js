242ea31102b5a04c42d45bfb4c6e5aef320eef6c
tranvandan0905 <dantranvan509@gmail.com>
2025-11-13 14:05:27 +0700

    add cors

--- server.js
+++ server.js
@@ -4,10 +4,15 @@ const connection = require('./config/database');
 const routeAPI = require('./routes/routeAPI');
 const cookieParser = require("cookie-parser");
 const app = express(); 
+const cors = require('cors');
 app.use(express.json()); 
 app.use(cookieParser()); 
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  
+}));
 app.use('/api', routeAPI);
-
 (async () => {
   try {
     await connection();
