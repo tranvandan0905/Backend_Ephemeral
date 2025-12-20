b6e93053ce4842ff2583cd253c0407809b40e77a
tranvandan0905 <dantranvan509@gmail.com>
2025-12-21 01:00:47 +0700

    update

---
+++ models/like.model.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const likeSchema = new mongoose.Schema({
+  userId:{ type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post", required: true },
+},{ timestamps: true });
+module.exports = mongoose.model('Like', likeSchema);
