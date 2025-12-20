4a2c3aa3c1fc5f18d2d1835ec3a6421d120fd4b6
tranvandan0905 <dantranvan509@gmail.com>
2025-12-20 21:00:11 +0700

    update

--- services/membership.service.js
+++ services/membership.service.js
@@ -2,7 +2,6 @@ const Membership = require("../models/membership.model");
 const FriendRequest = require("../models/friendrequest.model");
 const Friend = require("../models/friends.model");
 const { findRoomID, UpdateRoom } = require("./room.service");
-const { FindIDUser } = require("./user.service");
 const bcrypt = require("bcryptjs");
 const FindMembershipRoomID = async (roomId, userId) => {
   const room = await findRoomID(roomId);
@@ -52,33 +51,38 @@ const FindMembershipRoomID = async (roomId, userId) => {
       .map(r => r.senderId.toString())
   );
 
-  // Build response
   return members.map(m => {
-    const targetId = m.userId._id.toString();
-    let checkfriend = "NONE";
+  const targetId = m.userId._id.toString();
+  let checkfriend = "NONE";
 
-    if (targetId === userId.toString()) {
-      checkfriend = "SELF";
-    } else if (friendSet.has(targetId)) {
-      checkfriend = "FRIEND";
-    } else if (sentSet.has(targetId)) {
-      checkfriend = "SENT";
-    } else if (receivedSet.has(targetId)) {
-      checkfriend = "RECEIVED";
-    }
+  if (targetId === userId.toString()) {
+    checkfriend = "SELF";
+  } 
+  else if (friendSet.has(targetId)) {
+    checkfriend = "FRIEND";
+  } 
+  else if (sentSet.has(targetId)) {
+    checkfriend = "SENT";
+ 
+  } 
+  else if (receivedSet.has(targetId)) {
+    checkfriend = "RECEIVED";
+   
+  }
 
-    return {
-      userId: m.userId._id,
-      displayName: m.userId.displayName,
-      avatarUrl: m.userId.avatarUrl,
-      roomId: m.roomId,
-      role: m.role,
-      expiresAt: m.expiresAt,
-      status: m.status,
-      joinedAt: m.joinedAt,
-      checkfriend
-    };
-  });
+  return {
+    userId: m.userId._id,
+    displayName: m.userId.displayName,
+    avatarUrl: m.userId.avatarUrl,
+    roomId: m.roomId,
+    role: m.role,
+    expiresAt: m.expiresAt,
+    status: m.status,
+    joinedAt: m.joinedAt,
+    checkfriend,
+    receiverId:m.userId._id
+  };
+});
 };
 
 const findMembershipUserID = async (userId, roomId) => {
@@ -88,7 +92,7 @@ const findMembershipUserID = async (userId, roomId) => {
 };
 const createMembership = async (userId, roomId, password) => {
     const room = await findRoomID(roomId);
-    const user = await FindIDUser(userId);
+
 
     if (room.isPrivate) {
         const isMatch = await bcrypt.compare(password, room.passwordHash);
